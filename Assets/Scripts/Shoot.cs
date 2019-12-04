7a11a41407e11dedc0ffaff904037f1214a2fefb
KIRK WALLS <KIRWALLS@uat.edu>
2019-12-04 12:27:51 -0700

    Second Commit

---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    private Transform tf;
+    public GameObject pf;
+    public int lengthOfTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tf = GetComponent<Transform>();
+        pf = GameObject.Find("Ball");
+        StartCoroutine(Example());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator Example()
+    {
+        yield return new WaitForSecondsRealtime(2);
+        Shooting();
+
+        yield return new WaitForSecondsRealtime(2);
+        Shooting();
+
+    }
+
+    void Shooting()
+    {
+        //Shoot the bullet
+        GameObject newObject = Instantiate(pf, tf.position, tf.rotation) as GameObject;
+        Destroy(newObject, lengthOfTime);
+    }
+}
---
+++ Assets/Scripts/ShootingTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingTest : MonoBehaviour
+{
+    public Transform tf;
+
+    public Transform ShooterTF;
+
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tf = GetComponent<Transform>();
+        ShooterTF = GameObject.Find("Shooter").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tf.position -= tf.up * speed;
+
+    }
+}
