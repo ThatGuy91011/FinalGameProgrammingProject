63d134e46555b9b4b1ccaae7d1719095fddb227d
KIRK WALLS <KIRWALLS@uat.edu>
2019-12-09 12:08:08 -0700

    Third Commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Transform tf;
+
+    private int Position;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tf = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (tf.position.x == -23)
+        {
+            Position = 1;
+        }
+        else if (tf.position.x == -20)
+        {
+            Position = 2;
+        }
+        else if (tf.position.x == -17)
+        {
+            Position = 3;
+        }
+
+        if (Input.GetKeyDown((KeyCode.LeftArrow)))
+        {
+            if (Position != 1)
+            {
+                tf.position += new Vector3(-3, 0, 0);
+            }
+            else
+            {
+                tf.position += new Vector3(0, 0, 0);
+            }
+        }
+
+        if (Input.GetKeyDown((KeyCode.RightArrow)))
+        {
+            if (Position != 3)
+            {
+                tf.position += new Vector3(3, 0, 0);
+            }
+            else
+            {
+                tf.position += new Vector3(0, 0, 0);
+            }
+        }
+    }
+}
--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -24,15 +24,15 @@ public class Shoot : MonoBehaviour
     IEnumerator Example()
     {
         yield return new WaitForSecondsRealtime(2);
-        tf.Rotate(new Vector3(0,0,45));
+        tf.Rotate(new Vector3(0,0,40));
         Shooting();
 
         yield return new WaitForSecondsRealtime(2);
-        tf.Rotate(new Vector3(0, 0, -90));
+        tf.Rotate(new Vector3(0, 0, -80));
         Shooting();
 
         yield return new WaitForSecondsRealtime(2);
-        tf.Rotate(new Vector3(0, 0, 45));
+        tf.Rotate(new Vector3(0, 0, 40));
         Shooting();
 
     }
